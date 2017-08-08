a313b0e118f54dfe4c56399b9b779b2612d9558f
Siriuo <siriuo@outlook.com>
2017-08-08 17:23:00 +0100

    Added Announcement generator which will allow WireBot to send a message to the annoucements channel.

--- bot.js
+++ bot.js
@@ -5,19 +5,20 @@ const config = require('./config/config.json');
 const token = config['token'];
 const staffRoles = JSON.parse(JSON.stringify(config['staff_roles']));
 
+//channels
+
+
 client.on('ready', () => {
     console.log('I am listening!');
 });
 
 client.on('guildMemberAdd', member => {
-    // what should happen if someone joins
+    member.guild.channels.find('id', config['main_channel']).send("Welcome ${member}, Please check the " + member.guild.channels.find('id', config['welcome_channel']).mention + " channel for ");
 });
 
 client.on('message', message => {
 
     var userRole = message.member._roles[0];
-    console.log(userRole);
-    console.log(staffRoles);
     if (message.toString().startsWith("!" + "addcommand"))
     {
 
@@ -26,8 +27,23 @@ client.on('message', message => {
             console.log("User is not admin/staff");
             message.reply("You are not a staff member!");
         }else{
-            
+
+                message.reply('This feature does not exist yet. Please stand by.');
+
         }
+    }else if (message.toString().startsWith("!" + "announce")){
+        if(userRole.indexOf(staffRoles) > -1){
+            console.log("User is not admin/staff");
+            message.reply("You are not a staff member!");
+        }else{
+            var announcement = message.content.replace("!announce ", "");
+
+            message.guild.channels.find('id', config['announce_channel']).send(announcement);
+
+
+            message.delete(1);
+        }
+
     }
 
 });
